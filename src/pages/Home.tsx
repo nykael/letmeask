fabe8e13c2e934bc7442336e99e15099fee54be2
nykael <68310102+nykael@users.noreply.github.com>
2022-03-04 18:55:11 -0300

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,74 @@
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { FormEvent, useState } from 'react'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { database } from '../services/firebase'
+
 import { Button } from '../components/Button'
 import { useAuth } from '../hooks/useAuth'
-
-
 
 import '../style/auth.scss'
 
 // webpack (snowpack, vite)
 
 export function Home() {
-  const history = useHistory();
-  const { user, singnInWithGoogle} = useAuth()
-  
+  const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode] = useState('')
 
   async function HandleCreateRoom() {
-    if(!user) {
-     await singnInWithGoogle()
+    if (!user) {
+      await signInWithGoogle()
     }
 
-    history.push('/rooms/new');
-
+    history.push('/rooms/new')
   }
 
-  return(
-    <div id='page-auth'>
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (roomCode.trim() == '') {
+      return
+    }
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if (!roomRef.exists()) {
+      alert('Room does not exist')
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`)
+  }
+
+  return (
+    <div id="page-auth">
       <aside>
-        <img src= {illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie a sla de Q&amp;a ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div className='main-content'>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={HandleCreateRoom} className='create-room'> 
-          <img src={googleIconImg} alt="Logo do google" />
+          <button onClick={HandleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do google" />
             Crie a sua sala com o google
           </button>
-          <div className='separator'>Ou entre em uma sala</div>
-          <form>
-            <input 
-             type="text"
-             placeholder='Digite o código da sala'
+          <div className="separator">Ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
-            <Button type='submit' >
-              Entrar na sala
-            </Button>
-
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
